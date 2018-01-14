8971e34bd620a06c049a59245bf912bfdeef41c1
nitram509 <maki@bitkings.de>
2018-01-14 01:45:39 +0100

    add blinky intro animation

--- arduino-src/temperator/temperator.ino
+++ arduino-src/temperator/temperator.ino
@@ -16,6 +16,8 @@ struct CmdMessage {
 };
 
 CRGB leds[NUM_LEDS];
+CHSV hsv[NUM_LEDS];
+
 Servo tempServo;
 int pos = 0;    // variable to store the servo position
 
@@ -24,20 +26,22 @@ void lightUp() {
   display.print(8888);
   display.drawColon(true);
   display.writeDisplay();
-  
-  for (int i=0; i<NUM_LEDS; ++i) 
+
+  for (int i = 0; i < NUM_LEDS; ++i)
     leds[i] = CRGB::Green;
   FastLED.show();
 }
 
 void setup() {
   tempServo.attach(SERVO_PIN);
-  Serial.begin(57600); 
+  Serial.begin(57600);
   FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
   display.begin(0x70);
-
-  // TODO: remove
-  //lightUp();
+  randomSeed(analogRead(0));
+  for (int i = 0; i < NUM_LEDS; ++i) {
+    hsv[i] = rgb2hsv_approximate(CRGB::Black);
+    hsv[i].saturation = 255;
+  }
 }
 
 void showTemperature(float temperatureValue) {
@@ -52,7 +56,7 @@ void showTemperature(float temperatureValue) {
 
 int getCommandVal(CmdMessage cmdMsg) {
   String valStr = "";
-  for (int i=0; i<4; i++) {
+  for (int i = 0; i < 4; i++) {
     if (isDigit(cmdMsg.value[i]) || '-' == cmdMsg.value[i]) {
       valStr += (char)cmdMsg.value[i];
     }
@@ -62,8 +66,8 @@ int getCommandVal(CmdMessage cmdMsg) {
 }
 
 void showCity(int index) {
-  for (int i=0; i<NUM_LEDS; i++) {
-    leds[i] = CRGB::Black;  
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black;
   }
   if (index >= 0 && index < NUM_LEDS) {
     leds[index] = CRGB::Red;
@@ -88,6 +92,36 @@ void showClock(int time) {
   display.writeDisplay();
 }
 
+void showIntro() {
+  unsigned long start = millis();
+  int ticks = 0;
+  while (millis() - start < 4900) {
+    for (int i = 0; i < NUM_LEDS; ++i) {
+      hsv[i].value = max(0, hsv[i].value > 0 ? hsv[i].value - 2 : 0);
+      hsv2rgb_rainbow(hsv[i], leds[i]);
+    }
+    if (ticks % 25 == 0) {
+      int index = random(10);
+      if (hsv[index].val == 0) {
+        hsv[index].hue = random(256);
+        hsv[index].val = 255;
+      }
+
+    }
+    FastLED.show();
+    delay(5);
+    ticks++;
+  }
+  for (int t = 0; t < 128; ++t) {
+    for (int i = 0; i < NUM_LEDS; ++i) {
+      hsv[i].value = max(0, hsv[i].value > 0 ? hsv[i].value - 2 : 0);
+      hsv2rgb_rainbow(hsv[i], leds[i]);
+    }
+    FastLED.show();
+    delay(5);
+  }
+}
+
 bool isTemperatureCommand(CmdMessage cmdMsg) {
   return 't' == cmdMsg.command[0] && 'm' == cmdMsg.command[1] && 'p' == cmdMsg.command[2];
 }
@@ -98,6 +132,10 @@ bool isCityCommand(CmdMessage cmdMsg) {
 
 bool isClockCommand(CmdMessage cmdMsg) {
   return 'c' == cmdMsg.command[0] && 'l' == cmdMsg.command[1] && 'k' == cmdMsg.command[2];
+}
+
+bool isIntroCommand(CmdMessage cmdMsg) {
+  return 'd' == cmdMsg.command[0] && 'o' == cmdMsg.command[1] && 'i' == cmdMsg.command[2];
 }
 
 void loop() {
@@ -114,6 +152,9 @@ void loop() {
     if (isClockCommand(cmdMsg)) {
       showClock(getCommandVal(cmdMsg));
     }
+    if (isIntroCommand(cmdMsg)) {
+      showIntro();
+    }
   }
-  
+
 }
